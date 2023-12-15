0f01aee33fd4ae2dd2d55d619b81200a5e33b217
manolioluigi <luigi.manolio1999@gmail.com>
2023-12-15 13:07:10 +0100

    creazione primi component

--- frontend/src/components/Public/CategoryList.jsx
+++ frontend/src/components/Public/CategoryList.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CategoryList = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch('http://localhost:3300/categories');
+                if (response.ok) {
+                    const data = await response.json();
+                    setCategories(data);
+                } else {
+                    console.error('Error fetching categories:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching categories:', error.message);
+            }
+        };
+
+        fetchCategories();
+    }, []);
+
     return (
         <div>
-            <h1>CategoryList</h1>
+            <h2>Categories</h2>
+            <ul>
+                {categories.map((category) => (
+                    <li key={category.id}>{category.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
