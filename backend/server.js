f25177d5152aed0eda802a8245d84ed54aae378b
manolioluigi <luigi.manolio1999@gmail.com>
2023-12-15 11:28:31 +0100

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const server = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3300;
@@ -7,6 +8,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 const router = require('./router');
 
+server.use(cors());
 server.use(bodyParser.json());
 server.use('/', router);
 
--- frontend/src/components/Public/PhotoList.jsx
+++ frontend/src/components/Public/PhotoList.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+// components/Public/PhotoList.jsx
+import React, { useState, useEffect } from 'react';
 
 const PhotoList = () => {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        const fetchPhotos = async () => {
+            try {
+                const response = await fetch('http://localhost:3300/photos');
+                if (response.ok) {
+                    const data = await response.json();
+                    setPhotos(data);
+                } else {
+                    console.error('Error fetching photos:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error fetching photos:', error.message);
+            }
+        };
+
+        fetchPhotos();
+    }, []);
+
     return (
         <div>
-            <h1>PhotoList</h1>
+            <h2>Photo List</h2>
+            <ul>
+                {photos.map((photo) => (
+                    <li key={photo.id}>
+                        <p>{photo.title}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
